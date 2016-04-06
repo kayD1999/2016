e8971d7539796b3a3048239d3c4e6ba12af05e26
ccyD1999 <1348609460@qq.com>
2016-04-06 23:08:22 +0800

    11111

--- Gruntfile-最简单grunt.js
+++
@@ -1,7 +0,0 @@
-/**
- *  @ Author: 智能社-strive
- *  @ Date: 2016/4/5.
- */
-module.exports=function(grunt){
-    console.log('welcome grunt');
-};
--- dest/js/a.js
+++
@@ -1,1 +0,0 @@
-window.onload=function(){function show(arg1,arg2){alert(arg1+arg2)}document.onclick=function(){show(12,5)}};
